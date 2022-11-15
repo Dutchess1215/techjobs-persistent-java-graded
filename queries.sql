76b2731d74e23c86bca24adf22ba4670108e06ed
Nathan Murray <namurray1215@gmail.com>
2022-11-14 19:20:39 -0600

    1 test to go

--- queries.sql
+++ queries.sql
@@ -8,7 +8,8 @@ WHERE location = "St. Louis City";
 DROP TABLE job;
 
 -- Part 4: Test it with SQL dunno
-SELECT DISTINCT skill.name
-FROM job_skills
-LEFT JOIN skill ON skill.id=job_skills.skills_id
-ORDER BY skill.name;
+SELECT *
+FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
